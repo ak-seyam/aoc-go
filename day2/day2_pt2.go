72e834d1ab4d762ec9528478f9fa9f3ebe8c9c6d
A-Siam <abdullah.khaled.kamal0@gmail.com>
2022-12-05 07:29:34 +0200

    init day 2 pt 2

---
+++ day2/day2_pt2.go
@@ -0,0 +1,15 @@
+package day2
+
+import (
+	"strings"
+
+	"github.com/A-Siam/aoc-go/utils"
+)
+
+func Solution_pt2(inputPath string) {
+	utils.GetInput(inputPath, func(line string) {
+		inputs := strings.Split(line, " ")
+		opInp := inputs[0]
+		yourInp := inputs[1]
+	})
+}
